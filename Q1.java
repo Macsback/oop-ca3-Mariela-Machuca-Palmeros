2a9c2d7c2cece038c21e868fc8031c6d35e58be9
Macsback <119421402+Macsback@users.noreply.github.com>
2024-01-24 10:33:10 +0000

    Q1 - First Advances
    Started working in Q1, added the push method to add new car

--- Q1.java
+++ Q1.java
@@ -1,2 +1,38 @@
+
+
+import java.io.*;
+import java.util.*;
+
 public class Q1 {
+
+
+    public static void actions(Stack<Integer> park, Stack<Integer> street){
+        Scanner kbrd = new Scanner(System.in);
+        System.out.println("Input license No.");
+        int selection = kbrd.nextInt();
+
+        while(selection != 0){
+            if(selection>0){
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> park = new Stack<Integer>();
+        Stack<Integer> street = new Stack<Integer>();
+    }
+
+    void push(Stack<Integer> park, int car)
+    {
+        park.push(car);
+
+    }
+
+    void pop(Stack<Integer> park, int car)
+    {
+     
+
+    }
 }
