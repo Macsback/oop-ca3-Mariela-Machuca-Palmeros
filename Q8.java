30a85897a7ce20e8636778b4b3666f5508775c5e
Macsback <119421402+Macsback@users.noreply.github.com>
2024-02-06 10:50:37 +0000

    Continued Q8

--- Q8.java
+++ Q8.java
@@ -8,13 +8,15 @@ import java.io.InputStreamReader;
 public class Q8 {
 
     public static void main(String[] args) {
-        Stack<Integer> numbers= new Stack<Integer>();
+        Stack<Double> numbers = new Stack<Double>();
         Stack<String> operators = new Stack<String>();
 
         Scanner kbrd = new Scanner(System.in);
         String function = "";
 
-        System.out.println("Input one Operator or Number by line, Q to quit");
+
+
+        System.out.println("Input one Operator or Number by line, Q to quit, and = to calculate");
 
         do {
             function = kbrd.nextLine().toLowerCase();
@@ -22,18 +24,28 @@ public class Q8 {
             if (function.matches("\\d+")) {
 //                    System.out.println("This is an integer " + function);
 
-                int number = Integer.valueOf(function);
+                double number = Double.valueOf(function);
 
                 numbers.push(number);
+
+                System.out.println(numbers.peek());
 
 
 
             }
 
+
+
+            else if(function.equals("=")){
+                System.out.println("Calculating");
+               double result = calculate(numbers, operators);
+                System.out.println(result);
+            }
+
             else {
 //                    System.out.println("This is not an integer " + function);
                 operators.push(function);
-
+                System.out.println(operators.peek());
             }
 
         }
@@ -58,6 +70,26 @@ public class Q8 {
 //        while(function.contains("done"));
     }
 
+    public static double calculate(Stack<Double> numbers, Stack<String> operators) {
+
+
+        double num1 = 0;
+        double num2 = 0;
+        double result = 0;
+        if (numbers.size()>1 && !operators.isEmpty()){
+            String operator = operators.pop();
+            switch (operator){
+                case "+":
+                    num1 = numbers.pop();
+                    num2 = numbers.pop();
+                    result = num1 + num2;
+
+                    break;
+            }
+        }
+
+        return result;
+    }
 
 
 // TODO Make Scanner read one by one the parts of the operation !!
