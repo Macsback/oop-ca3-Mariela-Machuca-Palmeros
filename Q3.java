788a39991385ace0d8e1e633b66d5747d1cd3b05
Macsback <119421402+Macsback@users.noreply.github.com>
2024-02-08 11:36:50 +0000

    Started Q3

--- Q3.java
+++ Q3.java
@@ -1,2 +1,9 @@
 public class Q3 {
+
+
 }
+
+// Create a map (key) name of the identifier (value) Arraylist containing the lines where the identifiers are in
+// Scan through lines for identifiers, when Identifier is found check your map in case it exists
+// if it exists you just add the line into the arraylist of that identifier
+// if it doesnt you add it to the map and create a new key and arraylist for it with the line where it spotted it
