6590ca26f94921ffb4359684e512280106f1c96e
Macsback <119421402+Macsback@users.noreply.github.com>
2024-02-10 23:31:40 +0000

    Started and Finished Q2

--- Q2.java
+++ Q2.java
@@ -1,3 +1,104 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Q2 {
 
+    public static void main(String[] args) {
+        Scanner kbrd = new Scanner(System.in);
+        pair points = new pair();
+        int[][] arr = createArray();
+        System.out.println("Starting Array: \n");
+       display(arr);
+//        createArray();
+        System.out.println("Where would you like to start?");
+        System.out.println("Enter the column: ");
+        points.setCol(kbrd.nextInt());
+        System.out.println("Enter the Row: ");
+        points.setRow(kbrd.nextInt());
+        floodfill(points.getRow(), points.getCol(), arr ,points );
+        System.out.println("The Array has been filled: ");
+        display(arr);
+    }
+
+    public static int[][] createArray(){
+        int[][] arr = new int[10][10];
+        for (int i = 0; i < 10; i++)
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                arr[i][x] = 0;
+            }
+        }
+        return arr;
+    }
+
+    public static void display(int[][] arr){
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                System.out.printf("%4d", arr[x][y]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void floodfill(int row, int column, int[][] arr, pair points){
+        Stack<pair> cord = new Stack<>();
+        int count = 1;
+        cord.push(points);
+
+        while (!cord.isEmpty()){
+            pair current = cord.pop();
+            int c = current.col;
+            int r = current.row;
+
+            if(c>= 0 && c < 10 && r>=0 && r<10 && arr[r][c]==0){
+//                if(arr[r][c]==0){
+                arr[r][c] = count;
+                    count++;
+
+
+                    //Up
+
+                    if(r-1>=0 && arr[r-1][c] == 0){
+                        cord.push(new pair(r-1, c));
+//                        current.setRow(r-1);
+//                        current.setCol(c);
+//                        cord.push(current);
+                    }
+                    //Right
+
+                    if(c+1<10 && arr[r][c+1] == 0){
+//                        current.setRow(r);
+//                        current.setCol(c+1);
+//
+//                        cord.push(current);
+
+                        cord.push(new pair(r, c+1));
+                    }
+                    //Down
+
+                    if(r+1<10 && arr[r+1][c] == 0){
+//                        current.setRow(r+1);
+//                        current.setCol(c);
+//
+//                        cord.push(current);
+
+                        cord.push(new pair(r+1, c));
+                    }
+                    //Left
+
+                    if(c-1>= 0 && arr[r][c-1] == 0){
+//                        current.setRow(r);
+//                        current.setCol(c-1);
+//
+//                        cord.push(current);
+
+                        cord.push(new pair(r, c-1));
+                    }
+//                }
+            }
+        }
+    }
 }
---
+++ pair.java
@@ -0,0 +1,36 @@
+public class pair {
+
+
+        public int row;
+        public int col;
+
+        pair(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+
+    pair()
+    {
+        this.row = 0;
+        this.col = 0;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+}
+
